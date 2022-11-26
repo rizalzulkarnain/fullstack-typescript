c9cc05c9a8b7a306dcc93ce2c644ac577db88c8e
Rizal Zulkarnain <63713214+rizalzulkarnain@users.noreply.github.com>
2022-11-26 16:16:41 +0700

    initial commit

---
+++ client/src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,28 @@
+import React, { FC, ReactElement } from 'react';
+
+import { Grid } from '@mui/material';
+
+const Sidebar: FC = (): ReactElement => {
+  return (
+    <Grid
+      item
+      md={4}
+      sx={{
+        height: '100vh',
+        position: 'fixed',
+        right: 0,
+        top: 0,
+        width: '100%',
+        backgroundColor: 'background.paper',
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h2>SideBar Area</h2>
+    </Grid>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -0,0 +1,16 @@
+import React, { FC, ReactElement } from 'react';
+
+import { Grid } from '@mui/material';
+import TaskArea from '../../components/taskArea/TaskArea';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+const Dashboard: FC = (): ReactElement => {
+  return (
+    <Grid container minHeight="100vh" p={0} m={0}>
+      <TaskArea />
+      <Sidebar />
+    </Grid>
+  );
+};
+
+export default Dashboard;
